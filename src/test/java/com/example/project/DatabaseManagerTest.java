edf1f1720f033f2374259a44c79122600e28430f
Kyle Hagood <Hagood.Kyle@gmail.com>
2017-08-04 11:11:04 -0400

    Adding headers for database unit tests

---
+++ src/test/java/com/example/project/DatabaseManagerTest.java
@@ -0,0 +1,34 @@
+package com.example.project;
+
+import org.junit.Test;
+
+public class DatabaseManagerTest {
+    @Test
+    public void createTablesTest() throws Exception {
+
+    }
+    @Test
+    public void addUserTest() throws Exception {
+
+    }
+    @Test
+    public void removeUserTest() throws Exception {
+
+    }
+    @Test
+    public void addBuddyToUserTest() throws Exception {
+
+    }
+    @Test
+    public void removeBuddyFromTest() throws Exception {
+
+    }
+    @Test
+    public void comparePasswordSaltTest() throws Exception {
+
+    }
+    @Test
+    public void getBuddyListTest() throws Exception {
+        
+    }
+}
