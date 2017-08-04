6d2fd0a8e23ac85b81a512047bebe09bede4fe2f
Kyle Hagood <Hagood.Kyle@gmail.com>
2017-08-04 15:15:27 -0400

    Skipping authentication due to database bug involving storing non-ASCII characters

--- src/main/java/com/example/project/ChatThread.java
+++ src/main/java/com/example/project/ChatThread.java
@@ -125,7 +125,12 @@ public class ChatThread implements Runnable {
             oos.flush();
 
             userCredentials = (UserCredentials) ois.readObject();
-            if (databaseManager.comparePasswordSaltedHash(userCredentials.getUsername(), userCredentials.getPasswordSaltedHash())) {
+            //if (databaseManager.comparePasswordSaltedHash(userCredentials.getUsername(), userCredentials.getPasswordSaltedHash())) {
+            /*
+                Commenting out authentication check for now. Some password salt values are not being stored into
+                the database correctly due to non-ASCII characters. Needs fixed to finish authentication.
+             */
+            if (true) {
                 userCredentials.setRequestAccepted(true);
                 oos.writeObject(userCredentials);
                 oos.flush();
